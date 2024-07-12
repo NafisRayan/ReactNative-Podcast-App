d9a4d4e58aecb22995255a83972239439f5e026a
Nafis Rayan <nafisrayan123@gmail.com>
2024-07-13 02:27:16 +0600

    just works

--- babel.config.js
+++ babel.config.js
@@ -1,30 +1,6 @@
-module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-    'module:react-native-dotenv',
-  ],
-  plugins: [
-    ['@babel/plugin-transform-flow-strip-types'],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    [
-      'babel-plugin-root-import',
-      {
-        rootPathSuffix: 'src',
-      },
-    ],
-  ],
-  env: {
-    production: {
-      plugins: [
-        ['@babel/plugin-transform-flow-strip-types'],
-        ['@babel/plugin-proposal-class-properties', { loose: true }],
-        [
-          'babel-plugin-root-import',
-          {
-            rootPathSuffix: 'src',
-          },
-        ],
-      ],
-    },
-  },
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
 };
--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import { SERVER_URL } from 'react-native-dotenv';
-
-const api = axios.create({
-  baseURL: SERVER_URL,
-});
-
-export default api;
